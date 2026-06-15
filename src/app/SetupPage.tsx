8c6c641a33bd5562e46aaacc392de80b8ab7a923
Prince (Kofi Frimpong) Amissah <princeamissah0@gmail.com>
2026-06-15 14:28:28 +0000

    feat: add LiveLayer SVG identity mark

--- src/app/SetupPage.tsx
+++ src/app/SetupPage.tsx
@@ -56,7 +56,7 @@ export default function SetupPage() {
         <header className="cmd-bar">
           <div className="cmd-bar__left">
             <span className="cmd-logo">
-              <span className="cmd-logo__dot" aria-hidden />
+              <img className="cmd-logo__mark" src="/livelayer-mark.svg" alt="" aria-hidden="true" />
               LiveLayer
             </span>
             <span className="cmd-divider" aria-hidden />
